f603ec7a87ea61a9bd63e431a37561f17eca20a6
Alex Sergeev <sanhokk@yandex.ru>
2024-08-01 18:07:20 +0300

    some fixes

--- presentation/src/main/java/ru/alexsergeev/presentation/ui/utils/DomainEventToUiEventMapper.kt
+++ presentation/src/main/java/ru/alexsergeev/presentation/ui/utils/DomainEventToUiEventMapper.kt
@@ -8,15 +8,6 @@ interface Mapper<in I, out O> {
     fun map(input: I): O
 }
 
-interface ListMapper<in I, out O> : Mapper<List<I>, List<O>>
-
-class ListMapperImpl<in I, out O>(
-    private val mapper: Mapper<I, O>
-) : ListMapper<I, O> {
-    override fun map(input: List<I>): List<O> =
-        input.map(mapper::map)
-}
-
 internal class DomainEventToUiEventMapper(
     private val domainPersonToUiPersonMapper: DomainPersonToUiPersonMapper
 ) : Mapper<EventDomainModel, EventUiModel> {
